27deb2f10d86ade372a51663443aa5ca87a45021
tronghieuh446-prog <tronghieuh446@gmail.com>
2026-07-22 23:03:19 +0930

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from theme import set_title, loading, draw
+from terminal import terminal
+
+def main():
+    set_title()
+    loading()
+    draw()
+    terminal()
+
+if __name__ == "__main__":
+    main()
